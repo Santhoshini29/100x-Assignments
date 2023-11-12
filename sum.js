8749da91dc81170c44e79512b7e196abc92e0ce6
Santhoshini <adumalapellisanthoshini29@gmail.com>
2023-11-12 07:18:16 +0530

    added

--- sum.js
+++ sum.js
@@ -1,4 +1,10 @@
-function sumAll(startngindex,endingindex){
+function sumAll(startIndex,endIndex){
 
-
+var sum = 0;
+for (var i= startIndex;i<=endIndex;i++){
+    sum=sum+i;
 }
+return sum;
+}
+var sum = sumAll(1,4);
+console.log(sum);
