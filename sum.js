a8472fe83497b5d32b68dfcb7ffbd78548671a66
Santhoshini <adumalapellisanthoshini29@gmail.com>
2023-11-19 02:44:28 +0530

    changed

--- sum.js
+++ sum.js
@@ -1,20 +1,6 @@
-function getMedicine1() {
-    return new Promise(function(resolve, reject) {
-      setTimeout(function() {
-        console.log("1 Received")
-        resolve();
-      }, 1000);
-    });
-  }
-  
-  function getMedicine2() {
-    return new Promise(function(resolve, reject) {
-      setTimeout(function() {
-              console.log("2 Received")
-        resolve();
-      }, 2000);
-    });
-  }
-  
-  getMedicine1()
-      .then(getMedicine2())
+function printContent(error, data) {
+	console.log(data);
+}
+
+fs.read("file.txt", printContent) // Callback Version
+fs.read("file.txt").then(printContent) // Promise Version
