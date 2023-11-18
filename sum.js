837c13bcf7c5e2e2d761f7a7bd990fe56c59f761
Santhoshini <adumalapellisanthoshini29@gmail.com>
2023-11-19 02:36:24 +0530

    changed

--- sum.js
+++ sum.js
@@ -1,1 +1,20 @@
-// sum of 2 numbers 
+function getMedicine1() {
+    return new Promise(function(resolve, reject) {
+      setTimeout(function() {
+        console.log("1 Received")
+        resolve();
+      }, 1000);
+    });
+  }
+  
+  function getMedicine2() {
+    return new Promise(function(resolve, reject) {
+      setTimeout(function() {
+              console.log("2 Received")
+        resolve();
+      }, 2000);
+    });
+  }
+  
+  getMedicine1()
+      .then(getMedicine2())
