34ffc8ea79b3846143170b832138d6f1488dbaa0
Santhoshini <adumalapellisanthoshini29@gmail.com>
2023-11-18 20:34:34 +0530

    changed

--- counter.js
+++ counter.js
@@ -1,2 +1,7 @@
-
-HELLO 
+function calculate(a: number, b: number, type: string): number{
+	if(type === 'sum') return a + b;
+	if(type === 'sub') return a - b;
+	if(type === 'mul') return a * b;
+	if(type === 'div') return a / b;
+	return -1
+}
